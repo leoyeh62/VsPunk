e2daa0b4108961a29f538b8cda82a33d112670c6
Martin Bacqueville <martin_bacqueville@ens.univ-artois.fr>
2025-12-17 15:17:27 +0100

    Ajout du système de filtrage et des liens pour les articles d'une caractéristique donnée

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,20 +3,53 @@
 namespace App\Http\Controllers;
 
 use App\Models\Article;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Cookie;
 
 class ArticleController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $articles = Article::with([
-            'editeur',
-            'rythme',
-            'accessibilite'
-        ])
+        $searchT = trim($request->input('searchT', ''));
+        $searchA = trim($request->input('searchA', ''));
+        $searchR = trim($request->input('searchR', ''));
+        $searchAccess = trim($request->input('searchAccess', ''));
+        Cookie::queue('search', $searchT, 10);
+
+        $articles = Article::with(['editeur', 'rythme', 'accessibilite'])
+            ->when($searchT !== '', fn ($q) =>
+            $q->where('titre', 'like', "%$searchT%")
+            )
+            ->when($searchA !== '', fn ($q) =>
+            $q->whereHas('editeur', fn ($q2) =>
+            $q2->where('name', 'like', "%$searchA%")
+            )
+            )
+            ->when($searchR !== '', fn ($q) =>
+            $q->whereHas('rythme', fn ($q2) =>
+            $q2->where('texte', 'like', "%$searchR%")
+            )
+            )
+            ->when($searchAccess !== '', fn ($q) =>
+            $q->whereHas('accessibilite', fn ($q2) =>
+            $q2->where('texte', 'like', "%$searchAccess%")
+            )
+            )
             ->where('en_ligne', 1)
-            ->latest()
             ->get();
 
-        return view('articles.index', compact('articles'));
+        $titres = Article::select('titre')->distinct()->get();
+
+        return view('articles.index', [
+            'titre' => "Liste des articles",
+            'searchT' => $searchT,
+            'searchA' => $searchA,
+            'searchR' => $searchR,
+            'searchAccess' => $searchAccess,
+            'titres' => $titres,
+            'articles' => $articles
+        ]);
     }
 }
