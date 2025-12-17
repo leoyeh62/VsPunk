5f44d7cd1f0fffd863e8de7c98115f777355e6f1
Jean-Baptiste Hie <jean-baptiste_hie@ens.univ-artois.fr>
2025-12-17 14:05:45 +0100

    Ajout du compteur de vues

--- app/Http/Controllers/ArticleShowController.php
+++ app/Http/Controllers/ArticleShowController.php
@@ -8,12 +8,15 @@ class ArticleShowController extends Controller
 {
     public function show(int $id)
     {
+
         $article = Article::with([
             'editeur',
             'rythme',
             'accessibilite',
             'conclusion'
         ])->findOrFail($id);
+
+        $article->increment('nb_vues');
 
         return view('articles.show', compact('article'));
     }
